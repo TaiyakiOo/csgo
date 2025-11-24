aa21897865b3f487a4d962eccbc63421a3cb1064
TaiyakiOo <wyl1980676119@gmail.com>
2025-11-24 17:54:51 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const gameHistory = [
 
     // 示例数据：包含奖品字段
     { 
-        id: "第001期", 
+        id: "第000期", 
         seed: "CSGO-8888", 
         prize: "AWP | 二西莫夫 (久经沙场)", // <--- 这里是奖品
         players: ["玩家A", "玩家B", "玩家C", "玩家D", "玩家E"], 
@@ -15,3 +15,4 @@ const gameHistory = [
     // 把新数据粘贴在下面...
 
 ];
+
