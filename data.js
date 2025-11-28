4a9d27f1e2bb8457fd26b525236a684c47cf7c7e
TaiyakiOo <wyl1980676119@gmail.com>
2025-11-28 17:05:41 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 // --- 🔴 预告位设置 ---
-const teaser = { active: true, id: "第010期", prize: "P250 | 迷人幻象 (久经沙场)", date: "即将上线" };
+const teaser = { active: false, id: "第010期", prize: "P250 | 迷人幻象 (久经沙场)", date: "即将上线" };
 
 // --- 🔵 历史回放列表 ---
 const gameHistory = [
@@ -47,9 +47,12 @@ const gameHistory = [
 
     { id: "第008期", prize: "AWP | 死神 (久经沙场)", seed: "Random_6183", players: ["东方树叶🍃","东方树叶🍃","CaseGo3979588","what","东方树叶🍃"], outSequence: [1,2,0,3], winnerIdx: 4, date: "2025-11-27" },
 
-    { id: "第009期", prize: "柠檬汁胸章", seed: "Random_2728", players: ["what","what","七七七","七七七","七七七"], outSequence: [3,2,1,4], winnerIdx: 0, date: "2025-11-28" }
+    { id: "第009期", prize: "柠檬汁胸章", seed: "Random_2728", players: ["what","what","七七七","七七七","七七七"], outSequence: [3,2,1,4], winnerIdx: 0, date: "2025-11-28" },
+
+    { id: "第010期", prize: "P250 | 迷人幻象 (久经沙场)", seed: "Random_7126", players: ["CaseGo3624888","七七七","摸鱼爽","兮玫ovo","Ggboog"], outSequence: [1,4,3,2], winnerIdx: 0, date: "2025-11-28" }
 
 ];
+
 
 
 
