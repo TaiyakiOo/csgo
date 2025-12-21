58f1e18e15b114b01c620f0b713f6463d93f43ac
TaiyakiOo <wyl1980676119@gmail.com>
2025-12-21 19:17:45 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,21 @@
 // data.js
 
-const teaser = { active: false, id: "第048期", prize: "待定", date: "即将上线" };
+const teaser = { active: false, id: "第051期", prize: "？？？", date: "等待生成" };
 
 const gameHistory = [
-    // ⬇️ 这是一个自动猎杀的演示数据
+    
+    // 示例：前7个房间都被清洗，只有最后2个房间安全
     { 
-        id: "第047期-自动寻路测试", type: "map", 
-        prize: "M9刺刀 | 自动化 (略有磨损)", 
-        players: ["苏生", "七七七", "Liar", "东方树叶", "Ggboog", "CaseGo888"], 
-        rooms: [4, 0, 8, 2, 6, 4], // 分布在不同房间
-        outSequence: [5, 1, 3, 2], // 淘汰顺序
+        id: "第000期-无差别清洗演示", 
+        type: "map_zone", 
+        prize: "M4A1-S | 二西莫夫 (久经沙场)", 
+        players: ["A", "B", "C", "D", "E"], 
+        rooms: [0, 4, 8, 0, 2], // 玩家分布
+        huntSequence: [1, 3, 5, 7, 8, 2, 6], // 屠夫依次清洗这7个房间 (注意：虽然4号房和0号房没人被点到，但他们安全吗？不，只要不在序列里就安全)
+
         date: "2025-12-21" 
     },
+
+    // ⬇️ 粘贴你的 Admin 代码 ⬇️
     
-    // 你可以在这里粘贴 Admin 生成的新数据
 ];
