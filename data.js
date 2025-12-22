ef21b7a4fc8d283f5bae25727380a36bd69dab85
TaiyakiOo <wyl1980676119@gmail.com>
2025-12-22 13:23:05 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,24 @@
 // data.js
 
-const teaser = { active: false, id: "第051期", prize: "？？？", date: "等待生成" };
+const teaser = { active: false, id: "第053期", prize: "？？？", date: "等待生成" };
 
 const gameHistory = [
     
-    // 示例：前7个房间都被清洗，只有最后2个房间安全
+    // 演示局：追猎者 Admin 选择了 R1 和 R9 作为安全区
     { 
-        id: "第000期-无差别清洗演示", 
-        type: "map_zone", 
-        prize: "M4A4 | 二西莫夫 (久经沙场)", 
+        id: "第052期-猎杀行动", 
+        type: "map_hunter", 
+        prize: "M9刺刀 | 自动化", 
+        hunter: "红烧肉", // 追猎者名字
         players: ["A", "B", "C", "D", "E"], 
-        rooms: [0, 4, 8, 0, 2], // 玩家分布
-        huntSequence: [1, 3, 5, 7, 8, 2, 6], // 屠夫依次清洗这7个房间 (注意：虽然4号房和0号房没人被点到，但他们安全吗？不，只要不在序列里就安全)
-
-        date: "2025-12-21" 
+        rooms: [0, 4, 8, 0, 2], // 玩家分布: 苏生(1号), 七七(5号), Liar(9号), 东方(1号), Gg(3号)
+        // 安全区是 R1(0) 和 R9(8)。
+        // 意味着除了 0 和 8，其他都要被清洗。
+        // HuntSequence 必须包含 1,2,3,4,5,6,7 (乱序)
+        huntSequence: [4, 2, 6, 7, 3, 5, 1], 
+        date: "2025-12-22" 
     },
 
-    // ⬇️ 粘贴你的 Admin 代码 ⬇️
+    // 在这里粘贴 Admin 生成的代码
     
 ];
-
